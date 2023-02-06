ecfa47816c6b58a1fe46a134c8a6fe532968b5ea
Oleevay <vivhi.rs@gmail.com>
2023-02-06 18:31:09 +0100

    creation de product.js

---
+++ front/js/product.js
@@ -0,0 +1,42 @@
+const product = window.location.search;
+let urlParams = new URLSearchParams(product);
+let id = urlParams.get("id");
+
+// On récupère uniquement le produit dont on a besoin via le paramètre dans la requête
+fetch(`http://localhost:3000/products/${id}`)
+  // Quand tu as la réponse donne le résultat en json.
+  .then(
+    (reponse) =>reponse.json()
+        // Ce que tu as reçu et qui a été traité en json sera appelé data
+        .then((data) => {
+          handleProducts(data);
+        })
+    //Sort moi les informations en console sur ce qui ta été demandé
+  );
+
+function handleProducts(resultatApi) {
+  let addSofa = resultatApi;
+  let parentElement = document.querySelector("#.items_img");
+  parentElement.appendChild(imageElement)
+  let imageElement = document.querySelector("img");
+  imageElement.src = addSofa.imageUrl;
+  imageElement.alt = addSofa.altTxt;
+
+  let nameElement = document.querySelector("#title");
+  nameElement.innerText = addSofa.name;
+
+  let pElement = document.querySelector("#description");
+  pElement.innerText = addSofa.description;
+
+  let spanElement = document.querySelector("#price");
+  spanElement.innerText = addSofa.price;
+  //let priceElement = document.querySelector("#price");
+
+  let colorSelect = document.getElementById("#colors");
+  for (let i = 0; i < addSofa.color.length; i++) {
+    let option = document.createElement("option");
+    option.value = color;
+    option.innerText = addSofa.color;
+    colorSelect.appendChild(option);
+  }
+}
