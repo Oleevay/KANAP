76df90e90bd565f7dbee40256c63fd29b951f88a
Oleevay <vivhi.rs@gmail.com>
2023-02-13 18:25:44 +0100

    creation de la condition

--- front/js/products.js
+++ front/js/products.js
@@ -1,56 +1,66 @@
+//
 const queryString = window.location.search;
 let params = new URLSearchParams(queryString);
 let id = params.get("id");
 
 // On récupère uniquement le produit dont on a besoin via le paramètre dans la requête
 fetch(`http://localhost:3000/api/products/${id}`)
-  // Quand tu as la réponse donne le résultat en json.
+  // Quand il aura la réponse donne le résultat en json.
   .then((reponse) =>
     reponse
       .json()
-      // Ce que tu as reçu et qui a été traité en json sera appelé data
+      // Ce que qu'il a reçu et qui a été traité en json sera appelé data
       .then((data) => {
-        //Sort moi les informations en console sur ce qui ta été demandé
+        // On sort les informations en console sur ce qui ta été demandé
         handleAllProducts(data);
       })
   );
 
+// On rattache les data reçues par l'api au bon endroit
 function handleAllProducts(resultatApi) {
   let addSofa = resultatApi;
   image(addSofa);
   title(addSofa);
   price(addSofa);
   description(addSofa);
- color(addSofa);
+  color(addSofa);
+  addToCart();
 
 }
-
+//on crait la function image
 function image(addSofa) {
+  //on selectionne l'id de item__img
   let parentElement = document.querySelector(".item__img");
+  //On crait lélément image
   let imageElement = document.createElement("img");
   imageElement.src = addSofa.imageUrl;
   imageElement.alt = addSofa.altTxt;
-parentElement.appendChild(imageElement);
+  //on ajoute
+  parentElement.appendChild(imageElement);
 }
 
+//je crée la fonction title et
 function title(addSofa) {
   let nameElement = document.querySelector("#title");
   nameElement.innerText = addSofa.name;
 }
 
+//je crée la function pour afficher le prix
 function price(addSofa) {
   let spanElement = document.querySelector("#price");
   spanElement.innerText = addSofa.price;
 }
 
+//je crée la function
 function description(addSofa) {
   let pElement = document.querySelector("#description");
   pElement.innerText = addSofa.description;
 }
 
+//je crée la function
 function color(addSofa) {
   let colorSelect = document.getElementById("colors");
-  for (let i = 0; i <addSofa.colors.length;i++) {
+  for (let i = 0; i < addSofa.colors.length; i++) {
     let option = document.createElement("option");
     option.value = color;
     option.innerText = addSofa.colors[i];
@@ -58,7 +68,33 @@ function color(addSofa) {
   }
 }
 
+//création de la fonction addToCart
 function addToCart() {
+  //Gestion des bouttons
   const btn = document.querySelector("#addToCart");
-  btn.addEventListener("click", (event) => console.log("click!"));
+  //Evénement sur le btn lors du click
+  btn.addEventListener("click", (event) => {
+  //console.log("click!"));........
+  
+  //Récuperation de lélement du Dom pour les couleurs
+ const color = document.querySelector("#colors").value;
+ //Récuperation de lélement du Dom pour les quantitées
+  const quantity = document.querySelector("#quantity").value;
+  //Condition lors de la selection de la quantitée et de la couleur
+  if ( quantity < 1 || quantity > 100 && color == null){
+    alert ("veuillez selectionner la couleur et la quantité")
+  }
+
+  //
+})
 }
+
+
+//
+
+
+
+
+
+
+//
